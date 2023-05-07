fcd84929d5778e27d4c074c2cf5848266554b2ac
emanuelediluzio <48859533+emanuelediluzio@users.noreply.github.com>
2023-05-07 16:58:07 +0200

    Update index.js
    Changing the output file to .JSON due to problems with the .CSV format

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 const fs = require('fs');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const scrape = require('aliexpress-product-scraper');
-
-const csvWriter = createCsvWriter({
-  path: 'output.csv',
-  header: [
-    { id: 'title', title: 'Title' },
-    { id: 'price', title: 'Price' },
-    { id: 'description', title: 'Description' },
-    // Aggiungi altri campi che desideri includere nel CSV
-  ]
-});
 
 const product = scrape('32958933105');
 
 product.then(res => {
-  const data = {
-    title: res.title,
-    price: res.price,
-    description: res.description,
-    // Aggiungi altri campi che desideri includere nel CSV
-  };
-
-  csvWriter
-    .writeRecords([data])
-    .then(() => console.log('I dati sono stati scritti correttamente nel file output.csv'))
-    .catch(err => console.error('Errore nella scrittura del file CSV:', err));
+  const jsonData = JSON.stringify(res, null, 2);
+  fs.writeFile('output.json', jsonData, 'utf8', (err) => {
+    if (err) {
+      console.error('Errore nella scrittura del file:', err);
+    } else {
+      console.log('I dati sono stati scritti correttamente nel file output.json');
+    }
+  });
 });
